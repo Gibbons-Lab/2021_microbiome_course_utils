a2fae307b00223f6e39cc9293762066f0cc5737c
Christian Diener <ch.diener@gmail.com>
2021-09-22 14:44:30 -0700

    add bootstrap

--- course/bootstrap.py
+++ course/bootstrap.py
@@ -15,7 +15,7 @@ def am_i_colab():
         os.path.exists("/content") and
         os.environ.get("HOME") == "root" and
         "DATALAB_SETTINGS_OVERRIDES" in os.environ and
-        os.environ.get("GCE_METADATA_TIMEOUT") == 0
+        os.environ.get("GCE_METADATA_TIMEOUT") == "0"
     )
     return state
 
@@ -25,6 +25,7 @@ def main():
     if am_i_colab():
         download(prodigal_url, "/usr/local/bin/prodigal")
         os.chmod("/usr/local/bin/prodigal", 755)
+        con.print(":hammer: Set up [green]prodigal[/green].")
     else:
         con.print(
             ":fearful: This does not look like Colab, I better not touch anything.")
