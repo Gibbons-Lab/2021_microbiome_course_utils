b07db9c1bf53f27e1df049f3f43c5475b8d7e460
Christian Diener <ch.diener@gmail.com>
2021-09-28 18:08:29 -0700

    revert due to issue

--- course/utils.py
+++ course/utils.py
@@ -143,8 +143,6 @@ def gimme_exchanges():
     con.print(
         "Okay, summarized all exchange fluxes to [green]all_exchanges.csv[/green].")
     exc = pd.read_csv("all_exchanges.csv")
-    exc["genus"] = exc.species.str.split(" ").str[0]
-    exc.set_index("assembly", inplace=True)
     return exc
 
 
